d5bcaa5433932305a134eea6b8d933d8c0204c0a
ugo best <ugo16okafor@gmail.com>
2025-03-31 04:32:00 +0100

    fixed some kind bugs

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,7 +3,7 @@ import { Link, Element } from "react-scroll";
 
 const Projects = () => {
   return (
-    <section name ="projects">
+    <section name="projects">
       <div className="mx-10 py-14 font-Montserrat flex-col lg:flex-col justify-center ">
         <div className="flex justify-center text-2xl text-blue-500 py-5">
           <h1>Featured Projects</h1>
@@ -13,7 +13,7 @@ const Projects = () => {
             title="Portfolio Website"
             description="A personal portfolio showcasing my projects and skills."
             image="/assets/port.png"
-            link="https://ugodev.xyz"
+            link="ugodevs.vercel.app"
           />
         </div>
       </div>
